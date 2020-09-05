88d9947c7a6e8e46df6c76d8957ad9fd7f3edc77
Aryan Sharma <52833999+aryxns@users.noreply.github.com>
2020-09-05 13:37:43 +0530

    Add files via upload

---
+++ gensum.py
@@ -0,0 +1,24 @@
+import streamlit as st
+from gensim.summarization.summarizer import summarize
+from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
+from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
+from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
+
+query = st.text_area("Input here")
+
+text = query
+
+# Object of automatic summarization.
+auto_abstractor = AutoAbstractor()
+# Set tokenizer.
+auto_abstractor.tokenizable_doc = SimpleTokenizer()
+# Set delimiter for making a list of sentence.
+auto_abstractor.delimiter_list = [".", "\n"]
+# Object of abstracting and filtering document.
+abstractable_doc = TopNRankAbstractor()
+# Summarize document.
+result_dict = auto_abstractor.summarize(text, abstractable_doc)
+
+# Output result.
+for sentence in result_dict["summarize_result"]:
+    st.write(sentence)
